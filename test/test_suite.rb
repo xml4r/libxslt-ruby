a3a99f52f58b1fdc7a111b1aac056254fbf89e48
Charlie Savage <cfis@zerista.com>
2011-04-18 00:55:03 -0600

    When running tests set the working directory to the test directory.

--- test/test_suite.rb
+++ test/test_suite.rb
@@ -1,4 +1,10 @@
 # encoding: UTF-8
+
+# Change to current directory so relative
+# requires work.
+dir = File.dirname(__FILE__)
+Dir.chdir(dir)
+
 require 'test_libxslt'
 require 'test_stylesheet'
 require 'test_deprecated'
