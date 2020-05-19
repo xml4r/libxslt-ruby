f969756c68a5cc671232b4cdc1d893cb18c71eda
Charlie Savage <cfis@savagexi.com>
2020-05-19 01:47:15 -0700

    Update test helper to use bundler.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,5 @@
 # encoding: UTF-8
-# To make testing/debugging easier, test within this source tree versus an installed gem
 
-dir = File.dirname(__FILE__)
-root = File.expand_path(File.join(dir, '..'))
-lib = File.expand_path(File.join(root, 'lib'))
-
-$LOAD_PATH << lib
-
-begin
-  require 'xslt'
-rescue LoadError
-  require 'rubygems'
-  require 'xslt'
-end
+require 'bundler/setup'
+require 'minitest/autorun'
+require 'libxslt-ruby'
