94f2ed6d76b64c295a65c82d6a6d935a14c3be41
Charlie Savage <cfis@savagexi.com>
2020-05-19 01:48:37 -0700

    Remove deprecated methods.

--- lib/libxslt.rb
+++ lib/libxslt.rb
@@ -1,16 +1,3 @@
 # encoding: UTF-8
 
-# Load the libxml bindings
-require 'libxml'
-
-# Load the C-based binding.
-begin
-  RUBY_VERSION =~ /(\d+.\d+)/
-  require "#{$1}/libxslt_ruby"
-rescue LoadError
-  require "libxslt_ruby"
-end
-
-# And add support for deprecated functions
-require 'libxslt/deprecated'
-require 'libxslt/stylesheet'
+require 'libxslt-ruby'
