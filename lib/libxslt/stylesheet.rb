3c5c6015ea3656da9c03318477c9dbddf5f2205f
Charlie Savage <cfis@savagexi.com>
2020-05-19 01:47:50 -0700

    Use def self.<method_name> instead of class << self

--- lib/libxslt/stylesheet.rb
+++ lib/libxslt/stylesheet.rb
@@ -3,23 +3,25 @@ module LibXSLT
     class Stylesheet
       # options to be used for parsing stylesheets
       PARSE_OPTIONS = LibXML::XML::Parser::Options::NOCDATA | LibXML::XML::Parser::Options::NOENT
-      class << self
-        # create a xslt stylesheet from a string
-        def string(xml)
-          doc = LibXML::XML::Parser.string(xml, :options => PARSE_OPTIONS).parse
-          return new(doc)
-        end
-        # create a xslt stylesheet from a file specified by its filename
-        def file(filename)
-          doc = LibXML::XML::Parser.file(filename, :options => PARSE_OPTIONS).parse
-          return new(doc)
-        end
-        # create a xslt stylesheet from an io object
-        def io(io_object)
-          doc = LibXML::XML::Parser.io(io_object, :options => PARSE_OPTIONS).parse
-          return new(doc)
-        end
+
+      # create a xslt stylesheet from a string
+      def self.string(xml)
+        doc = LibXML::XML::Parser.string(xml, :options => PARSE_OPTIONS).parse
+        return new(doc)
       end
+
+      # create a xslt stylesheet from a file specified by its filename
+      def self.file(filename)
+        doc = LibXML::XML::Parser.file(filename, :options => PARSE_OPTIONS).parse
+        return new(doc)
+      end
+
+      # create a xslt stylesheet from an io object
+      def self.io(io_object)
+        doc = LibXML::XML::Parser.io(io_object, :options => PARSE_OPTIONS).parse
+        return new(doc)
+      end
+
       # transform a xml to a string
       def transform(doc)
         return output(apply(doc))
